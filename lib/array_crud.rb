78782129c1b31390f4f6c1e40fdf9a8b7faeecb5
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-24 06:39:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,34 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  co_workers = ["Mike V.", "Mike M.", "Troy", "Antonio"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array = ["Wow", "I", "am", "really", "learning"]
+ array << "arrays!"
+ #array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [ "I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+   array = ["I", "am", "really", "learning", "arrays!"]
+   arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   
 end
 
